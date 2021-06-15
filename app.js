8e006d2777d401b5ddb1a5b19c032f335a41fe6c
aya-alhusamia <alhusamiaya70@gmail.com>
2021-06-15 14:21:33 +0300

    create and delete  the route

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 const express = require("express");
 const products = require("./data");
 const app = express();
+app.use(express.json());
+//  Create products route
+app.post("/products", (req, res) => {
+  req.body.id = products[products.length - 1].id + 1;
+  products.push(req.body);
+  res.status(201).json(req.body);
+});
 
+//Delete products route
+app.delete("/products/:productID", (req, res) => {
+  const findProduct = products.find(
+    (product) => product.id === +req.params.productID
+  );
+  if (findProduct) {
+    products.filter((product) => product.id !== +req.params.productID);
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+// Pruduct list
 app.get("/products", (req, res) => {
   console.log(products);
   res.json(products);
 });
+
+// Delaile of products
+app.get("/products/:productID", (req, res) => {
+  const findProduct = products.find(
+    (product) => product.id === +req.params.productID
+  );
+  if (findProduct) {
+    res.json(findProduct);
+  } else {
+    res.status(404).end();
+  }
+});
+
 const PORT = 8000;
 app.listen(PORT, () => console.log(`${PORT}`));
